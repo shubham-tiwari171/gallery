a823ec568413372e30fc9b43fce16d139291f7ba
Shubham <tshubhamucem@gmail.com>
2023-06-22 17:11:32 +0530

    after some time user is not able to log in

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import Register from './components/atoms/Register/Register';
 import Login from './components/atoms/Login/Login';
 import { Pages } from './components/Pages/Pages';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 
 function App() {
   const { user, isLoggedIn } = useSelector((state) => state.user);
-  const location = useLocation();
   console.log(isLoggedIn)
+  const location = useLocation();
   const navigate = useNavigate();
 
+
+  // useEffect(() => {
+  //   // Check for logout
+  //   if (Object.keys(user).length === 0 && location.pathname !== '/login' && location.pathname !== '/register') {
+  //     navigate('/');
+  //   }
+  // }, [user, location.pathname, navigate]);
+
   useEffect(() => {
-    // logout quickely
-    if (Object.keys(user).length === 0 && location.pathname !== '/login' && location.pathname !== '/register') {
-      navigate('/login');
-    }
-    //check for login 
-    if (!isLoggedIn && location.pathname !== '/login' && location.pathname !== "/register") {
+    // Check for login timeout
+
+    if (!isLoggedIn && location.pathname !== '/login' && location.pathname !== '/register') {
       const timer = setTimeout(() => {
         navigate('/login');
-      }, 60000);
+      }, 10000);
       return () => clearTimeout(timer);
     }
-  }, [navigate, isLoggedIn]);
+  }, [isLoggedIn, location.pathname, navigate]);
 
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Pages />} />
+        <Route path="/" element={isLoggedIn ? <Pages /> : <Login />} />
         {!isLoggedIn && <Route path="/login" element={<Login />} />}
         <Route path="/register" element={<Register />} />
       </Routes>
